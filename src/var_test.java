205a3e009697a75114355d963fcf5cf859374ddb
kaitongchow <iezhouyukai@gmail.com>
2020-08-11 22:27:33 +0800

    var变量

---
+++ src/var_test.java
@@ -0,0 +1,8 @@
+public class var_test {
+    public static void main(String[] args){
+        //var动态类型变量，第一次赋值的变量类型就决定了以后这个变量的类型
+        var a = 12;//int
+        var b = 1.2;//float
+        var c = 1.6d;//double
+    }
+}
